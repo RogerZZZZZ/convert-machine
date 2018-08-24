cbb23338aa19ef8f0da615c57356ac0debdb1f9e
RogerZZZZZ <501841352@qq.com>
2018-08-24 11:41:46 +0800

    fix the webpack file lint problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
-var root = path.resolve(__dirname, './');
+const path = require('path');
+const webpack = require('webpack');
+
+const root = path.resolve(__dirname, './');
 
 module.exports = {
     devtool: 'source-map',
 
-    entry:  [
+    entry: [
         'webpack-hot-middleware/client?quiet=true',
-        __dirname + "/src/test.js",
+        path.join(__dirname, '/src/test.js'),
     ],
     output: {
-        path: __dirname + "/",
-        filename: "match.js"
+        path: path.join(__dirname, '/'),
+        filename: 'match.js',
     },
     resolve: {
-        alias: {
-           MATCH: path.resolve(__dirname, 'src/match'),
-           LIB: path.resolve(__dirname, 'src/lib'),
-           TEST: path.resolve(__dirname, 'src/test')
-        }
     },
     module: {
-        loaders: [
-        {
+        loaders: [{
             test: /\.json$/,
-            loader: "json-loader"
+            loader: 'json-loader',
         },
         {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            include: root
+            include: root,
         },
         {
             test: /\.css$/,
-            loader: 'style!css'//添加对样式表的处理
-        }
-        ]
+            loader: 'style!css',
+        }],
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: '"production"'
-            }
+                NODE_ENV: '"production"',
+            },
         }),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
     ],
 
     devServer: {
-        contentBase: "./src/",
+        contentBase: './src/',
         port: 9999,
         historyApiFallback: true,
-        inline: true
-    }
-}
+        inline: true,
+    },
+};
